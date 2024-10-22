66c3cf603e154b4b5863b956751bc5a106d1fd4e
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-10-22 22:11:44 +0700

    feat(memberships): add service and repository interface for user management

---
+++ internal/service/memberships/service.go
@@ -0,0 +1,21 @@
+package memberships
+
+import (
+	"context"
+	"github.com/mdafaardiansyah/forumista-backend/internal/model/memberships"
+)
+
+type membershipRepository interface {
+	GetUser(ctx context.Context, email, username string) (*memberships.UserModel, error)
+	CreateUser(ctx context.Context, model memberships.UserModel) error
+}
+
+type service struct {
+	membershipRepo membershipRepository
+}
+
+func NewService(membershipRepo membershipRepository) *service {
+	return &service{
+		membershipRepo: membershipRepo,
+	}
+}
