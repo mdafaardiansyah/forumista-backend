f2ef1352e10c7073e7f0c4be42f6719fe8e070de
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-10-22 21:34:02 +0700

    Delete Test Connection Database

--- internal/repository/memberships/repository.go
+++ internal/repository/memberships/repository.go
@@ -2,7 +2,6 @@ package memberships
 
 import (
 	"database/sql"
-	"log"
 )
 
 type repository struct {
@@ -10,21 +9,6 @@ type repository struct {
 }
 
 func NewRepository(db *sql.DB) *repository {
-	rows, err := db.Query("SELECT id, email FROM users")
-	if err != nil {
-		log.Println("error query", err)
-	}
-	defer rows.Close() //close the rows, for anticipated memory leaks
-
-	for rows.Next() {
-		var id int64
-		var email string
-		err = rows.Scan(&id, &email)
-		if err != nil {
-			log.Println("error scan", err)
-		}
-		log.Printf("id: %d, email: %s\n\n", id, email)
-	}
 	return &repository{
 		db: db,
 	}
