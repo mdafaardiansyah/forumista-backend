f40ad973da4f604b23d6a9b27d2d3aa7fadac69e
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-10-22 22:21:17 +0700

    feat(memberships): implement handler SignUp and routing for membership service POST Method

--- internal/handlers/memberships/handler_membership.go
+++ internal/handlers/memberships/handler_membership.go
@@ -1,20 +1,30 @@
 package memberships
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
+	"github.com/mdafaardiansyah/forumista-backend/internal/model/memberships"
 )
+
+type membeshipService interface {
+	SignUp(ctx context.Context, req memberships.SignUpRequest) error
+}
 
 type Handler struct {
 	*gin.Engine
+
+	membershipSvc membeshipService
 }
 
-func NewHandler(api *gin.Engine) *Handler {
+func NewHandler(api *gin.Engine, membershipSvc membeshipService) *Handler {
 	return &Handler{
-		Engine: api,
+		Engine:        api,
+		membershipSvc: membershipSvc,
 	}
 }
 
 func (h *Handler) RegisterRoute() {
 	route := h.Group("/memberships")
 	route.GET("/ping", h.Ping)
+	route.POST("/sign-up", h.SignUp)
 }
