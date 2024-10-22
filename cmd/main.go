0485b1618a27b62e4d4ea6df4dd9e60e9c179771
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-10-22 13:24:40 +0700

    Initiate the Configs in main.go

---
+++ cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mdafaardiansyah/forumista-backend/internal/configs"
+	"log"
+)
+
+func main() {
+	r := gin.Default()
+
+	var (
+		cfg *configs.Config
+	)
+
+	err := configs.Init(
+		configs.WithConfigFolder(
+			[]string{"./internal/configs"},
+		),
+		configs.WithConfigFile("config"),
+		configs.WithConfigType("yaml"),
+	)
+	if err != nil {
+		log.Fatal("Gagal inisialisasi Config")
+	}
+	cfg = configs.Get()
+	log.Println("config", cfg)
+
+	r.Run(cfg.Service.Port)
+}
