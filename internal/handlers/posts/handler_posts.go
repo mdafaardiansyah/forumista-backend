d3f4ac13733728adcf1dfe73ed63c80c6a7cad5c
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-11-02 00:08:49 +0700

    feat(user_activities): Add path and Contract to create user activity

--- internal/handlers/posts/handler_posts.go
+++ internal/handlers/posts/handler_posts.go
@@ -10,6 +10,7 @@ import (
 type postService interface {
 	CreatePost(ctx context.Context, userID int64, req posts.CreatePostRequest) error
 	CreateComment(ctx context.Context, postID, userID int64, request posts.CreateCommentRequest) error
+	UpsertUserActivity(ctx context.Context, postID, userID int64, request posts.UserActivityRequest) error
 }
 
 type Handler struct {
@@ -31,4 +32,6 @@ func (h *Handler) RegisterRoute() {
 
 	route.POST("/create", h.CreatePost)
 	route.POST("/comment/:postID", h.CreateComment)
+	route.PUT("/user_activity/:postID", h.UpsertUserActivity)
+
 }
