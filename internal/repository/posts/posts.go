7f8466421387a79b5f6d46d4f37d74e577986135
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-11-02 20:46:52 +0700

    feat(pagination): Create a repository for get post with pagination

--- internal/repository/posts/posts.go
+++ internal/repository/posts/posts.go
@@ -3,6 +3,7 @@ package posts
 import (
 	"context"
 	"github.com/mdafaardiansyah/forumista-backend/internal/model/posts"
+	"strings"
 )
 
 func (r *repository) CreatePost(ctx context.Context, model posts.PostModel) error {
@@ -12,4 +13,42 @@ func (r *repository) CreatePost(ctx context.Context, model posts.PostModel) erro
 		return err
 	}
 	return nil
+}
+
+func (r *repository) GetAllPost(ctx context.Context, limit, offset int) (posts.GetAllPostResponse, error) {
+	query := `SELECT p.id, p.user_id, u.username, p.post_title, p.post_content, p.post_hashtags 
+	FROM posts p JOIN users u ON p.user_id = u.id ORDER BY p.updated_at DESC LIMIT ? OFFSET ?`
+
+	response := posts.GetAllPostResponse{}
+	rows, err := r.db.QueryContext(ctx, query, limit, offset)
+	if err != nil {
+		return response, err
+	}
+	defer rows.Close()
+
+	data := make([]posts.Post, 0)
+	for rows.Next() {
+		var (
+			model    posts.PostModel
+			username string
+		)
+		err = rows.Scan(&model.ID, &model.UserID, &username, &model.PostTitle, &model.PostContent, &model.PostHashtags)
+		if err != nil {
+			return response, err
+		}
+		data = append(data, posts.Post{
+			ID:           model.ID,
+			UserID:       model.UserID,
+			Username:     username,
+			PostTitle:    model.PostTitle,
+			PostContent:  model.PostContent,
+			PostHashtags: strings.Split(model.PostHashtags, ","),
+		})
+	}
+	response.Data = data
+	response.Pagination = posts.Pagination{
+		Limit:  limit,
+		Offset: offset,
+	}
+	return response, nil
 }
