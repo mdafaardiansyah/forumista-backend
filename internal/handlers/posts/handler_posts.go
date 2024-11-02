b33a9d66c84a71d8c48c0aa6899b07c1c729b6e8
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-11-02 20:49:27 +0700

    feat(pagination): Add a path for the Get Post handler

--- internal/handlers/posts/handler_posts.go
+++ internal/handlers/posts/handler_posts.go
@@ -11,6 +11,7 @@ type postService interface {
 	CreatePost(ctx context.Context, userID int64, req posts.CreatePostRequest) error
 	CreateComment(ctx context.Context, postID, userID int64, request posts.CreateCommentRequest) error
 	UpsertUserActivity(ctx context.Context, postID, userID int64, request posts.UserActivityRequest) error
+	GetAllPost(ctx context.Context, pageSize, pageIndex int) (posts.GetAllPostResponse, error)
 }
 
 type Handler struct {
@@ -33,5 +34,6 @@ func (h *Handler) RegisterRoute() {
 	route.POST("/create", h.CreatePost)
 	route.POST("/comment/:postID", h.CreateComment)
 	route.PUT("/user_activity/:postID", h.UpsertUserActivity)
+	route.GET("/", h.GetAllPost)
 
 }
