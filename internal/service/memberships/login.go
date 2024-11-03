933a37fe8a3a3eec5225e16a523479648bc3b2cf
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-11-03 11:54:12 +0700

    feat(refresh_token): Integrate function to generate refresh token in Login, Signup and insert service

--- internal/service/memberships/login.go
+++ internal/service/memberships/login.go
@@ -5,32 +5,62 @@ import (
 	"errors"
 	"github.com/mdafaardiansyah/forumista-backend/pkg/jwt"
 	"golang.org/x/crypto/bcrypt"
+	"strconv"
+	"time"
 
 	"github.com/mdafaardiansyah/forumista-backend/internal/model/memberships"
+	tokenUtil "github.com/mdafaardiansyah/forumista-backend/pkg/token"
 	"github.com/rs/zerolog/log"
 )
 
-func (s *service) Login(ctx context.Context, req memberships.LoginRequest) (string, error) {
-	user, err := s.membershipRepo.GetUser(ctx, req.Email, "")
-
+func (s *service) Login(ctx context.Context, req memberships.LoginRequest) (string, string, error) {
+	user, err := s.membershipRepo.GetUser(ctx, req.Email, "", 0)
 	if err != nil {
-		log.Error().Err(err).Msg("Failed to get user")
-		return "", err
+		log.Error().Err(err).Msg("failed to get user")
+		return "", "", err
 	}
-
 	if user == nil {
-		return "", errors.New("email not exist")
+		return "", "", errors.New("email not exist")
 	}
 
 	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password))
 	if err != nil {
-		return "", errors.New("email or password is invalid")
+		return "", "", errors.New("email or password is invalid")
 	}
 
 	token, err := jwt.CreateToken(user.ID, user.Username, s.cfg.Service.SecretJWT)
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
-	return token, nil
 
+	existingRefreshToken, err := s.membershipRepo.GetRefreshToken(ctx, user.ID, time.Now())
+	if err != nil {
+		log.Error().Err(err).Msg("error get latest refresh token from database")
+		return "", "", err
+	}
+
+	if existingRefreshToken != nil {
+		return token, existingRefreshToken.RefreshToken, nil
+	}
+
+	refreshToken := tokenUtil.GenerateRefreshToken()
+	if refreshToken == "" {
+		return token, "", errors.New("failed to generate refresh token")
+	}
+
+	err = s.membershipRepo.InsertRefreshToken(ctx, memberships.RefreshTokenModel{
+		UserID:       user.ID,
+		RefreshToken: refreshToken,
+		ExpiredAt:    time.Now().Add(10 * 24 * time.Hour),
+		CreatedAt:    time.Now(),
+		UpdatedAt:    time.Now(),
+		CreatedBy:    strconv.FormatInt(user.ID, 10),
+		UpdatedBy:    strconv.FormatInt(user.ID, 10),
+	})
+	if err != nil {
+		log.Error().Err(err).Msg("error inserting refresh token to database")
+		return token, refreshToken, err
+	}
+
+	return token, refreshToken, nil
 }
--- internal/service/memberships/service.go
+++ internal/service/memberships/service.go
@@ -4,11 +4,14 @@ import (
 	"context"
 	"github.com/mdafaardiansyah/forumista-backend/internal/configs"
 	"github.com/mdafaardiansyah/forumista-backend/internal/model/memberships"
+	"time"
 )
 
 type membershipRepository interface {
-	GetUser(ctx context.Context, email, username string) (*memberships.UserModel, error)
+	GetUser(ctx context.Context, email, username string, userID int64) (*memberships.UserModel, error)
 	CreateUser(ctx context.Context, model memberships.UserModel) error
+	GetRefreshToken(ctx context.Context, userID int64, now time.Time) (*memberships.RefreshTokenModel, error)
+	InsertRefreshToken(ctx context.Context, model memberships.RefreshTokenModel) error
 }
 
 type service struct {
